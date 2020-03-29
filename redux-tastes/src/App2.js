4cd82392b4f5e478534e0b594f453a7a7276fd25
j.karp <j.karp@createit.com>
2020-03-29 20:10:52 +0200

    r-tastes-initial-commit

---
+++ redux-tastes/src/App2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App2() {
+  return (
+    <div className="App2">
+     Jestem druga
+    </div>
+  );
+}
+
+export default App2;
---
+++ redux-tastes/src/App3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App3() {
+  return (
+    <div className="App3">
+     Jestem trzecia
+    </div>
+  );
+}
+
+export default App3;
