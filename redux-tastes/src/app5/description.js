6b38c0406e1a81e4eaae54ea27d0d8001ff226e4
j.karp <j.karp@createit.com>
2020-09-18 09:52:49 +0200

    app5-with-react-redux-description

--- redux-tastes/src/app5/description.js
+++ redux-tastes/src/app5/description.js
@@ -4,7 +4,13 @@ import '../App.css';
 function Description() {
   return (
     <div className="description">
-      This is Redux
+      Pełna implementacja Reduxa. W App5.js aplikację opakowujemy providerem z pakietu react-redux.
+      Do niego jako propsa przekazujemy store. Store utwożony jest w odzielnym pliku - z pakietu redux wyciągamy funkcję createStore i jako argument podajemy reducer. 
+      Reducer w osobnym pliku. Tu jest zdefiniowany początkowy stan aplikacji. Reducer przyjmuje akcje. W zależności od typu akcji, aktualizuje stan korzystając z przychoidzącego z akcją paylodu.
+      Akcje w osobnym pliku - zdefiniowane jako stringi. (Akcje to obiekty opisujące zdarzenia/eventy - payload)
+      Kreatory akcji - w osobnym pliku - to funkcje, zwracające akcje.
+      Do komponentu stan jest przekazywany dzięki funkcjo connect z pakietu react-redux. Możemy przekazać wybrane elementy stanu dzięki state.element. Zwyczajowo poprzez mapStatetoProps.
+      Po podłączeniu componentu przez connect są w nim dostępne jako propsy - state oraz funkcja dispatch. Która przekazuje ("dispaczuje") creatory akcji do reducera.
     </div>
   );
 }
