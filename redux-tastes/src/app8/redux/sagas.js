7a4e9479cd3268801b23e7858ee73aa000a8e5a2
j.karp <j.karp@createit.com>
2020-10-07 09:16:42 +0200

    takeLatest-saga

--- redux-tastes/src/app8/redux/sagas.js
+++ redux-tastes/src/app8/redux/sagas.js
@@ -1,16 +1,15 @@
-import { call, fork, put, take, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import * as api from '../api';
 
 
 export default function* rootSaga() {
-  // yield takeLatest(watchFetchTasks);
-  yield fork(watchFetchTasks);
-  yield fork(watchElse);
+  yield takeLatest('FETCH_SMILE_STARTED', watchFetchTasks);
+  // yield fork(watchElse);
 };
 
 function* watchFetchTasks() {
-  while (true) {
-    yield take('FETCH_SMILE_STARTED')
+  // while (true) {
+    // yield take('FETCH_SMILE_STARTED')
     try {
       const gif = yield call(api.getSmile);
       yield put({
@@ -22,9 +21,9 @@ function* watchFetchTasks() {
         type: 'FETCH_SMILE_FAILED',
       })
     }
-  }
+  // }
 }
 
-function* watchElse() {
-  yield console.log('rooooooooooooooootSaga watching something else');
-}
+// function* watchElse() {
+//   yield console.log('rooooooooooooooootSaga watching something else');
+// }
